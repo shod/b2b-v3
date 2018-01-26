e37a58becef63922182aafd822189b6d3fbce859
Natali <natalja_kr@tut.by>
2018-01-26 15:59:16 +0200

    start price page

--- views/product/price.php
+++ views/product/price.php
@@ -1,1 +1,143 @@
-Работа с прайсом
+<?php
+$this->title = "Импорт/экспорт прайса";
+?>
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card" style="height: 100%">
+            <div class="card-header">
+                Мой прайс
+            </div>
+            <div class="card-block">
+                <div class="row">
+                    <div class="col-lg-2">
+                        <b>Файл</b>
+                    </div>
+                    <div class="col-lg-8">
+                        <input class="btn btn-primary" type="button" value="Скачать мой прайс-лист" id="btn_download_price" />
+                    </div>
+                    <div class="col-lg-2">
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-2">
+                        <b>Ссылка</b>
+                    </div>
+                    <div class="col-lg-8">
+                        <a style="width: 100%" href='http://www.migom.by/?block=mtex_seller_price&seller_id=$vars[seller_id]&key_str=$vars[md5_seller]&mode=csv' target='_blank'>http://www.migom.by/?block=mtex_seller_price&seller_id=$vars[seller_id]&key_str=$vars[md5_seller]&mode=csv</a>
+                    </div>
+                    <div class="col-lg-2">
+                        (csv формат)
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-2">
+                        <b>Ссылка</b>
+                    </div>
+                    <div class="col-lg-8">
+                        <a href='http://www.migom.by/?block=mtex_seller_price&seller_id=$vars[seller_id]&key_str=$vars[md5_seller]&mode=yml' target='_blank'>http://www.migom.by/?block=mtex_seller_price&seller_id=$vars[seller_id]&key_str=$vars[md5_seller]&mode=yml</a>
+                    </div>
+                    <div class="col-lg-2">
+                        (xml формат Яндекс Маркета)
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card" style="height: 100%">
+            <div class="card-header">
+                Шаблон прайса для обновления
+            </div>
+            <div class="card-block">
+                <p>Прайс-лист должен удовлетворять шаблону (<a href="http://files.migom.by/files/template-price-new.xlsx" type='application/excel'>скачать</a>)</p>
+
+
+                <form class="form-inline" method="get" id="frm_download_tmpl" >
+                     <input type="hidden" name="block" value="content_products" />
+                     <input type="hidden" name="action" value="export_catalog" />
+
+                     <input class="btn btn-primary" type="button" value="Скачать шаблон прайса" id="btn_download_tmpl"/>
+                     <span style="margin: 0px 10px">для</span>
+                     <select class="form-control" name="catalog_id" >
+                          <option value="0">всех товаров ($vars[cnt_all])</option>
+                          $vars[catalog_options]
+                     </select>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card" style="height: 100%">
+            <div class="card-header">
+                Закачать прайс-лист
+            </div>
+            <div class="card-block">
+
+
+                <div><b style='color:red'>Внимание!</b> Если вы используете в качестве прайс-листа выгрузку цен &nbsp;с площадки <b>Onliner.by</b>,
+                    название загружаемого файла должно быть <b>onliner.csv</b></div><br>
+
+                <div class="alert alert-danger ks-active-border" role="alert"><img src='http://admin.migom.by/ads/img/attention-ico.png'/> Перед закачкой прайса проверьте <b>соответствие</b> валюты в прайсе и в <a class="alert-link" href='/?admin=settings&&action=options' target="_blank">настройках</a>.</div>
+
+                <form method="post" id="frm_import" enctype="multipart/form-data">
+                    <input type="hidden" name="block" value="content_products" />
+                    <input type="hidden" name="action" value="import" />
+                    <input type="hidden" name="MAX_FILE_SIZE" value="15000000">
+
+                    <div class="row">
+                        <div class="col-lg-2">
+                            <b>Файл прайса:</b>
+                        </div>
+                        <div class="col-lg-10">
+                            <div class="ks-upload-block">
+                                <span class="ks-icon la la-cloud-upload"></span>
+                                <span class="ks-text">Перетащите файл сюда </span>
+                                <span class="ks-upload-btn">
+                                            <span class="ks-btn-separator">или</span>
+                                            <button class="btn ks-btn-file">
+                                                <span class="la la-cloud-upload ks-icon"></span>
+                                                <span class="ks-text">Выберите файл</span>
+                                                <input type="file">
+                                            </button>
+                                        </span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-2">
+                            <b>URL прайса:</b>
+                        </div>
+                        <div class="col-lg-10">
+                            <input class="form-control form-group-sm" type="text" name="url" value="$vars[url]" />
+                        </div>
+                    </div>
+                    <div class="content-end">
+                        <input class="btn btn-primary" type="button" value="Закачать" onclick="this.disabled=true;this.value='Подождите...';this.form.submit();"/>
+                    </div>
+
+
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card" style="height: 100%">
+            <div class="card-block">
+                $vars[results]
+            </div>
+        </div>
+    </div>
+</div>
--- views/site/index.php
+++ views/site/index.php
@@ -6,33 +6,34 @@ $this->title = "Добро пожаловать в b2b.migom.by!";
     <div class="col-lg-12">
         <div class="card card-block">
             <h3>Все страницы для работы</h3>
+            <div style="background-color:#b8f8b8">3 -  <a href="/balance/add">Баланс пополнить</a></div>
+            <div style="background-color:#b8f8b8">5 - <a href="/balance/akt">Баланс выгрузка актов</a></div>
+            <div style="background-color:#b8f8b8">11 - <a href="/order/sms">SMS заказы</a></div>
+            <div style="background-color:#b8f8b8">12 - <a href="/product/on-sale">Товары в продаже</a></div>
+            <div style="background-color:#b8f8b8">13 - <a href="/product/catalog">Редактирование товаров в разделе</a></div>
+            <div style="background-color:#b8f8b8">25 - <a href="/statistic/cost-analysis">Анализ цен конкурентов</a></div>
+            <div style="background-color:#b8f8b8">16 - <a href="/reviews">Отзывы</a></div>
+            <div style="background-color:#b8f8b8">22 - <a href="/statistic">Статистика</a></div>
+            <div style="background-color:#b8f8b8">23 - <a href="/statistic/month">Статистика по месяцам</a></div>
 
-            <p>1 - <a href="/auction">Аукционы</a></p>
-            <p>2 - <a href="/auction/add">Аукционы добавление</a></p>
-            <p style="background-color:#b8f8b8">3 -  <a href="/balance/add">Баланс пополнить</a></p>
-            <p>4 - <a href="/balance/promise">Баланс обещаный платех</a></p>
-            <p>5 - <a href="/balance/akt">Баланс выгрузка актов</a></p>
-            <p>6 - <a href="/balance/report">Баласн Финансовый отчет</a></p>
-            <p>7 - <a href="/context-adv">Контекстная реклама добавление</a></p>
-            <p>8 - <a href="/context-adv/blacklist">Контекстная реклама работа с разделами</a></p>
-            <p>9. <a href="/info">Информационная страница</a></p>
-            <p>10 - <a href="/news">Страница новостей</a></p>
-            <p style="background-color:#b8f8b8">11 - <a href="/order/sms">SMS заказы</a></p>
-            <p style="background-color:#b8f8b8">12 - <a href="/product/on-sale">Товары в продаже</a></p>
-            <p style="background-color:#b8f8b8">13 - <a href="/product/catalog">Редактирование товаров в разделе</a></p>
-            <p>14 - <a href="/product/price">Товары работа с прайсами</a></p>
-            <p style="background-color:#b8f8b8">25 - <a href="/statistic/cost-analysis">Анализ цен конкурентов</a></p>
-            <p>15 - <a href="/product/tariff">Калькулятор тарифов</a></p>
-            <p style="background-color:#b8f8b8">16 - <a href="/reviews">Отзывы</a></p>
-            <p>17. <a href="/seller">Продавец сводная информация</a></p>
-            <p>18 - <a href="/seller/settings">Продавец настройки</a></p>
-            <p>19 - <a href="/seller/delivery">Продавец информация о доставке</a></p>
-            <p>20 - <a href="/spec">Спецпредложения</a></p>
-            <p>21 - <a href="/spec/add">Спецпредложения добавление</a></p>
-            <p style="background-color:#b8f8b8">22 - <a href="/statistic">Статистика</a></p>
-            <p style="background-color:#b8f8b8">23 - <a href="/statistic/month">Статистика по месяцам</a></p>
-            <p>24. <a href="/banner-spec">Баннерные спецпредложения</a></p>
-            <p>25. <a href="/banner-spec/add">Баннерные спецпредложения добавление</a></p>
+            <div>1 - <a href="/auction">Аукционы</a></div>
+            <div>2 - <a href="/auction/add">Аукционы добавление</a></div>
+            <div>4 - <a href="/balance/promise">Баланс обещаный платех</a></div>
+            <div>6 - <a href="/balance/report">Баласн Финансовый отчет</a></div>
+            <div>7 - <a href="/context-adv">Контекстная реклама добавление</a></div>
+            <div>8 - <a href="/context-adv/blacklist">Контекстная реклама работа с разделами</a></div>
+            <div>9. <a href="/info">Информационная страница</a></div>
+            <div>10 - <a href="/news">Страница новостей</a></div>
+            <div>14 - <a href="/product/price">Товары работа с прайсами</a></div>
+            <div>15 - <a href="/product/tariff">Калькулятор тарифов</a></div>
+            <div>17. <a href="/seller">Продавец сводная информация</a></div>
+            <div>18 - <a href="/seller/settings">Продавец настройки</a></div>
+            <div>19 - <a href="/seller/delivery">Продавец информация о доставке</a></div>
+            <div>20 - <a href="/spec">Спецпредложения</a></div>
+            <div>21 - <a href="/spec/add">Спецпредложения добавление</a></div>
+
+            <div>24. <a href="/banner-spec">Баннерные спецпредложения</a></div>
+            <div>25. <a href="/banner-spec/add">Баннерные спецпредложения добавление</a></div>
         </div>
     </div>
 </div>
