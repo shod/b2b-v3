1f170bda9a1a0d786ec0d935f9ff6ee11897684e
Natali <natalja_kr@tut.by>
2018-05-08 14:06:35 +0300

    weather widget timer

--- components/widgets/views/weather/show.php
+++ components/widgets/views/weather/show.php
@@ -8,8 +8,28 @@
         </div>
     </div>
     <div class="ks-widget-weather-and-datetime-datetime-block">
-        <div class="ks-widget-weather-and-datetime-datetime-block-datetime">6:18 pm</div>
+        <div class="ks-widget-weather-and-datetime-datetime-block-datetime"><div id="clock"><span class="hour">hh</span>:<span class="min">mm</span></div></div>
         <div class="ks-widget-weather-and-datetime-datetime-block-location">Минск</div>
         <span style="font-size: 60px" class="ks-icon wi wi-wu-<?= strtolower($weather['weather'][0]['main']) ?>"></span>
     </div>
 </div>
+<script type="text/javascript">
+    function update() {
+        var clock = document.getElementById('clock');
+
+        var date = new Date(); // (*)
+
+        var hours = date.getHours();
+        if (hours < 10) hours = '0' + hours;
+        clock.children[0].innerHTML = hours;
+
+        var minutes = date.getMinutes();
+        if (minutes < 10) minutes = '0' + minutes;
+        clock.children[1].innerHTML = minutes;
+    }
+    function clockStart() { // запустить часы
+        timerId = setInterval(update, 1000);
+        update(); // (*)
+    }
+    clockStart();
+</script>
