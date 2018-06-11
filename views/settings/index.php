55f08c2c2db9e21a61a57181f3648a4e0f62af67
Natali <natalja_kr@tut.by>
2018-06-11 11:13:44 +0300

    buttons on user info page

--- views/settings/index.php
+++ views/settings/index.php
@@ -32,7 +32,7 @@ $this->registerJsFile(Yii::$app->request->baseUrl . '/web/scripts/js/settings.js
                         <label>телефон, факс</label>
                         <input name="fax" type="text"
                                class="form-control" <?= (isset($fax) && $fax != "") ? "readonly" : "" ?> placeholder=""
-                               value="<?= $fax ?>" data-validation="length" data-validation-length="min5"
+                               value="<?= (isset($fax) && $fax != "") ? $fax : "" ?>" data-validation="length" data-validation-length="min5"
                                data-validation-error-msg="Введите телефон, факс">
                     </div>
                 </div>
@@ -45,15 +45,15 @@ $this->registerJsFile(Yii::$app->request->baseUrl . '/web/scripts/js/settings.js
                             <tbody>
                             <tr>
                                 <td style="padding-left: 0px;"><span><small>индекс</small><br><input
-                                                class="form-control" <?= (isset($zip_code) && $zip_code != "") ? "readonly" : "" ?> type="text" name="zip_code" value="<?= $zip_code ?>" data-validation="length"
+                                                class="form-control" <?= (isset($zip_code) && $zip_code != "") ? "readonly" : "" ?> type="text" name="zip_code" value="<?= (isset($zip_code) && $zip_code != "") ? $zip_code : "" ?>" data-validation="length"
                                                 data-validation-length="min5" data-validation-error-msg="Введите индекс"></span>
                                 </td>
                                 <td><span><small>город</small><br><input class="form-control" type="text"
-                                                                         name="city" value="<?= $city ?>" <?= (isset($city) && $city != "") ? "readonly" : "" ?> data-validation="length"
+                                                                         name="city" value="<?= (isset($city) && $city != "") ? $city : "" ?>" <?= (isset($city) && $city != "") ? "readonly" : "" ?> data-validation="length"
                                                                          data-validation-length="min5" data-validation-error-msg="Введите город"></span></td>
                                 <td><span><small>улица, дом, корпус, офис</small><br><input class="form-control"
                                                                                             type="text" name="address"
-                                                                                            value="<?= $address ?>" <?= (isset($address) && $address != "") ? "readonly" : "" ?> data-validation="length"
+                                                                                            value="<?= (isset($address) && $address != "") ? $address : "" ?>" <?= (isset($address) && $address != "") ? "readonly" : "" ?> data-validation="length"
                                                                                             data-validation-length="min5" data-validation-error-msg="Введите улица, дом, корпус, офис"></span></td>
                             </tr>
                             </tbody>
@@ -65,16 +65,16 @@ $this->registerJsFile(Yii::$app->request->baseUrl . '/web/scripts/js/settings.js
                             <tbody>
                             <tr>
                                 <td style="padding-left: 0px;"><span><small>индекс</small><br><input
-                                                class="form-control red" type="text" name="zip_code_law" value="<?= $zip_code_law ?>" <?= (isset($zip_code_law) && $zip_code_law != "") ? "readonly" : "" ?> data-validation="length"
+                                                class="form-control red" type="text" name="zip_code_law" value="<?= (isset($zip_code_law) && $zip_code_law != "") ? $zip_code_law : "" ?>" <?= (isset($zip_code_law) && $zip_code_law != "") ? "readonly" : "" ?> data-validation="length"
                                                 data-validation-length="min5" data-validation-error-msg="Введите улица, дом, корпус, офис"></span>
                                 </td>
                                 <td><span><small>город</small><br><input class="form-control red" type="text"
-                                                                         name="city_law" value="<?= $city_law ?>" <?= (isset($city_law) && $city_law != "") ? "readonly" : "" ?> data-validation="length"
+                                                                         name="city_law" value="<?= (isset($city_law) && $city_law != "") ? $city_law : "" ?>" <?= (isset($city_law) && $city_law != "") ? "readonly" : "" ?> data-validation="length"
                                                                          data-validation-length="min5" data-validation-error-msg="Введите улица, дом, корпус, офис"></span></td>
                                 <td><span><small>улица, дом, корпус, офис</small><br><input class="form-control red"
                                                                                             type="text"
                                                                                             name="address_law"
-                                                                                            value="<?= $address_law ?>" <?= (isset($address_law) && $address_law != "") ? "readonly" : "" ?> data-validation="length"
+                                                                                            value="<?= (isset($address_law) && $address_law != "") ? $address_law : "" ?>" <?= (isset($address_law) && $address_law != "") ? "readonly" : "" ?> data-validation="length"
                                                                                             data-validation-length="min5" data-validation-error-msg="Введите улица, дом, корпус, офис"></span></td>
                             </tr>
                             </tbody>
@@ -90,28 +90,28 @@ $this->registerJsFile(Yii::$app->request->baseUrl . '/web/scripts/js/settings.js
                         <label>Название организации</label>
                         <input name="company_name" type="text"
                                class="form-control" <?= (isset($company_name) && $company_name != "") ? "readonly" : "" ?>
-                               placeholder="" value="<?= $company_name ?>" data-validation="length"
+                               placeholder="" value="<?= (isset($company_name) && $company_name != "") ? $company_name : "" ?>" data-validation="length"
                                data-validation-length="min5" data-validation-error-msg="Введите название организации">
                     </div>
                     <div class="col-lg-3">
                         <label>Ф.И.О. Директора</label>
                         <input name="fio_director" type="text"
                                class="form-control" <?= (isset($fio_director) && $fio_director != "") ? "readonly" : "" ?>
-                               placeholder="" value="<?= $fio_director ?>" data-validation="length"
+                               placeholder="" value="<?= (isset($fio_director) && $fio_director != "") ? $fio_director : "" ?>" data-validation="length"
                                data-validation-length="min5" data-validation-error-msg="Введите Ф.И.О. Директора">
                     </div>
                     <div class="col-lg-3">
                         <label>Действует на основании</label>
                         <input name="basis" type="text" class="form-control"
                                placeholder="" <?= (isset($basis) && $basis != "") ? "readonly" : "" ?>
-                               value="<?= $basis ?>" data-validation="length" data-validation-length="min5"
+                               value="<?= (isset($basis) && $basis != "") ? $basis : "" ?>" data-validation="length" data-validation-length="min5"
                                data-validation-error-msg="Введите информацию">
                     </div>
                     <div class="col-lg-3">
                         <label>УНП</label>
                         <input name="unn" type="text"
                                class="form-control" <?= (isset($unn) && $unn != "") ? "readonly" : "" ?> placeholder=""
-                               value="<?= $unn ?>" data-validation="length" data-validation-length="min5"
+                               value="<?= (isset($unn) && $unn != "") ? $unn : "" ?>" data-validation="length" data-validation-length="min5"
                                data-validation-error-msg="Введите УНП">
                     </div>
                 </div>
@@ -124,28 +124,28 @@ $this->registerJsFile(Yii::$app->request->baseUrl . '/web/scripts/js/settings.js
                         <label>наименование банка</label>
                         <input name="bank_name" type="text"
                                class="form-control" <?= (isset($bank_name) && stripcslashes($bank_name) != "" && stripcslashes($bank_name) != " ") ? "readonly" : "" ?>
-                               placeholder="" value="<?= stripcslashes($bank_name) ?>" data-validation="length"
+                               placeholder="" value="<?= (isset($bank_name) && stripcslashes($bank_name) != "" && stripcslashes($bank_name) != " ") ? stripcslashes($bank_name) : "" ?>" data-validation="length"
                                data-validation-length="min1" data-validation-error-msg="Введите наименование банка">
                     </div>
                     <div class="col-lg-3">
                         <label>код банка</label>
                         <input name="bank_code" type="text"
                                class="form-control" <?= (isset($bank_code) && $bank_code != "") ? "readonly" : "" ?>
-                               placeholder="" value="<?= stripcslashes($bank_code) ?>" data-validation="length"
+                               placeholder="" value="<?= (isset($bank_code) && $bank_code != "") ? stripcslashes($bank_code) : "" ?>" data-validation="length"
                                data-validation-length="min1" data-validation-error-msg="Введите код банка">
                     </div>
                     <div class="col-lg-3">
                         <label>расч. счет</label>
                         <input name="ras_schet" type="text"
                                class="form-control" <?= (isset($ras_schet) && $ras_schet != "") ? "readonly" : "" ?>
-                               placeholder="" value="<?= stripcslashes($ras_schet) ?>" data-validation="length"
+                               placeholder="" value="<?= (isset($ras_schet) && $ras_schet != "") ? stripcslashes($ras_schet) : "" ?>" data-validation="length"
                                data-validation-length="min1" data-validation-error-msg="Введите расч. счет">
                     </div>
                     <div class="col-lg-3">
                         <label>адрес банка</label>
                         <input name="bank_address" type="text"
                                class="form-control" <?= (isset($bank_address) && $bank_address != "") ? "readonly" : "" ?>
-                               placeholder="" value="<?= stripcslashes($bank_address) ?>" data-validation="length"
+                               placeholder="" value="<?= (isset($bank_address) && $bank_address != "") ? stripcslashes($bank_address) : "" ?>" data-validation="length"
                                data-validation-length="min1" data-validation-error-msg="Введите адрес банка">
                     </div>
                 </div>
