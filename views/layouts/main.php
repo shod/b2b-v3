5a69b4789661a919bd03029a03b8c993b1937ad9
Natali <natalja_kr@tut.by>
2018-08-22 10:28:57 +0300

    new layout

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -155,11 +155,12 @@ AppAsset::register($this);
                     </a>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="Preview" style="height: 235px;">
                         <ul class="nav nav-tabs ks-nav-tabs ks-info" role="tablist">
+
                             <li class="nav-item">
-                                <a class="nav-link active" href="#" style="margin: 10px;" data-toggle="tab" data-target="#ks-navbar-tech" role="tab">Служба технической поддержки</a>
+                                <a class="nav-link active" href="#" style="margin: 10px;" data-toggle="tab" data-target="#ks-navbar-users" role="tab">Ваш менеджер</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#" style="margin: 10px;" data-toggle="tab" data-target="#ks-navbar-users" role="tab">Ваш менеджер</a>
+                                <a class="nav-link" href="#" style="margin: 10px;" data-toggle="tab" data-target="#ks-navbar-tech" role="tab">Служба технической поддержки</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="#" style="margin: 10px;" data-toggle="tab" data-target="#ks-navbar-reviews" role="tab">Модератор отзывов</a>
@@ -167,7 +168,27 @@ AppAsset::register($this);
                         </ul>
                         <div class="tab-content">
 
-                            <div class="tab-pane ks-messages-tab active" id="ks-navbar-tech" role="tabpanel" style="height: 150px;">
+
+                            <div class="tab-pane ks-messages-tab active" id="ks-navbar-users" role="tabpanel" style="height: 150px;">
+                                <div class="ks-wrapper">
+                                    <div class="ks-message" style="height: 61%;">
+                                        <div class="">
+                                            <img src="http://b2b.migom.by/img/design/janna_admin.jpg" style="margin-right: 10px;" height="130">
+                                        </div>
+                                        <div class="ks-info">
+                                            <div class="ks-user-name"><h4>Жанна</h4></div>
+                                            <div class="ks-text">
+                                                <b>Тел:</b> +375 29 <span style="color:#AA0000; font-size: 13px;">111-45-45</span> (Velcom) <span class="viber-icon"></span><br>
+                                                <b>Skype:</b> sale.migom <br>
+                                                <b>E-mail:</b> <a href="mailto:sale@migom.by">sale@migom.by</a> <br>
+                                                <b>Время работы:</b> с 9:00 до 18:00
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="tab-pane ks-messages-tab " id="ks-navbar-tech" role="tabpanel" style="height: 150px;">
                                 <div class="ks-wrapper">
                                     <div class="ks-message" style="height: 61%;">
                                         <div class="">
@@ -179,25 +200,6 @@ AppAsset::register($this);
                                                 <b>Тел:</b> +375 29 <span style="color:#AA0000; font-size: 13px;">688-45-46</span> (Velcom)<br>
                                                 <b>Skype:</b> admin.migom <br>
                                                 <b>E-mail:</b> <a href="mailto:admin@migom.by">admin@migom.by</a> <br>
-                                                <b>Время работы:</b> с 9:00 до 18:00
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="tab-pane ks-messages-tab" id="ks-navbar-users" role="tabpanel" style="height: 150px;">
-                                <div class="ks-wrapper">
-                                    <div class="ks-message" style="height: 61%;">
-                                        <div class="">
-                                            <img src="http://b2b.migom.by/img/design/janna.jpg" style="margin-right: 10px;" height="130">
-                                        </div>
-                                        <div class="ks-info">
-                                            <div class="ks-user-name"><h4>Жанна</h4></div>
-                                            <div class="ks-text">
-                                                <b>Тел:</b> +375 29 <span style="color:#AA0000; font-size: 13px;">111-45-45</span> (Velcom) <span class="viber-icon"></span><br>
-                                                <b>Skype:</b> sale.migom <br>
-                                                <b>E-mail:</b> <a href="mailto:sale@migom.by">sale@migom.by</a> <br>
                                                 <b>Время работы:</b> с 9:00 до 18:00
                                             </div>
                                         </div>
