0295503f784207fd9d92b0c4c0a26a6db42c836a
Natali <natalja_kr@tut.by>
2018-08-30 11:41:22 +0300

    tour

--- components/widgets/views/promise/show.php
+++ components/widgets/views/promise/show.php
@@ -1,4 +1,4 @@
-<div class="card ks-widget-payment-price-ratio ks-purple">
+<div class="card ks-widget-payment-price-ratio ks-purple" style="background-color: rgb(242, 227, 233)">
     <div class="ks-price-ratio-title">
         Обещанный платеж
     </div>
