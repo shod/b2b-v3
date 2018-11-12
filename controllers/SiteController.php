e72fc5f6e077f35d51b3dc76553adb58d47debdd
Natali <natalja_kr@tut.by>
2018-11-12 14:25:42 +0200

    new passwords

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -163,7 +163,7 @@ class SiteController extends Controller
 
         $is_ads = (isset($_SERVER['HTTP_X_REAL_IP']) && ($_SERVER['HTTP_X_REAL_IP'] == '86.57.147.222'));
         if($is_ads){
-            $model->password = 'admin_lhe;,f78';
+            $model->password = 'pbvf_,kbprj18';
             if ($model->login()) {
                 $seller_id = Yii::$app->user->identity->getId();
                 $sql = "call pc_recovery_product_seller_data({$seller_id});";
