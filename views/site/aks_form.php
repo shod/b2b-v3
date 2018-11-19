984380644db202802fbab643a508aac607638d7a
Natali <natalja_kr@tut.by>
2018-11-19 10:40:49 +0200

    check if question

--- views/site/aks_form.php
+++ views/site/aks_form.php
@@ -1,8 +1,8 @@
 <h4>Заявки на добавления товаров, прочие вопросы</h4>
 <form id="question_form" method="post" action="/site/send-question">
     <input type="hidden" name="_csrf" value="<?= Yii::$app->request->getCsrfToken() ?>"/>
-    <textarea name="question" class="form-control" style="height: 200px;"></textarea>
+    <textarea id="seller_question" name="question" class="form-control" style="height: 200px;"></textarea>
 </form>
 <div class="content-end" style="padding-top: 20px;">
-    <button onclick="ajaxSubmit(this,'question_form')" class=" btn btn-primary">Отправить</button>
+    <button onclick="if($('#seller_question').val() != ''){ajaxSubmit(this,'question_form')}else{$.alert({title: 'Вы не задали вопрос!',type: 'blue',content: 'Для продолжения задайте вопрос и нажмите отправить или закройте форму.'});}" class=" btn btn-primary">Отправить</button>
 </div>
